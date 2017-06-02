da37934d575e53e04f79621ae6bfab0bcfef4ebf
aaronbrackett472 <aaronbrackett472@gmail.com>
2017-06-01 18:37:47 -0700

    fjaidfafj

--- Potential Games/battleship.py
+++ Potential Games/battleship.py
@@ -7,6 +7,8 @@ class State():
         self.playerBoard = initializeBoard()
         self.cpuBoard = initializeBoard()
         self.totalShips = 3#per side
+        self.playerShips = set([])
+        self.cpuShips = set([])
 
     def initializeBoard(self):
         board = []
@@ -14,17 +16,16 @@ class State():
             board.append(["O"] * 5)
         return board
 
-    def print_board(board):
-        for row in board:
-            print " ".join(row)
 
     def placeShip(coordinates, player):
         if player == 1:
             for coordinate in coordinates:
                 self.cpuBoard[coordinate[0]][coordinate[1]] = "S"
+                self.cpuShips.add(coordinate)
         elif player == 0:
             for coordinate in coordinates:
                 self.playerBoard[coordinate[0]][coordinate[1]] = "S"
+                self.playerShips.add(coordinate)
 
 class CPU():
     def __init__(self):
@@ -170,7 +171,14 @@ class CPU():
 
 #starting the game and printing the board
 print "Let's play Battleship!"
-print_board(board)
+
+state = State()
+cpu = CPU()
+cpu.placeShips(state)
+
+for i in range(0, self.totalShips):
+    inputList = raw_input("Where do you want your first ship to be?").split()
+    state.placeShip((inputList[0], inputList[1],inputList[2],inputList[3],inputList[4],inputList[5]), 0)
 
 #defining where the ship is
 def random_row(board):
@@ -179,40 +187,39 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
 
 #asking the user for a guess
-for turn in range(4):
+while True:
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
+    state.fir
 
-    # if the user's right, the game ends
-    if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
-        break
-    else:
-        #warning if the guess is out of the board
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+
+
+    # # if the user's right, the game ends
+    # if guess_row == ship_row and guess_col == ship_col:
+    #     print "Congratulations! You sunk my battleship!"
+    #     break
+    # else:
+    #     #warning if the guess is out of the board
+    #     if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+    #         print "Oops, that's not even in the ocean."
         
-        #warning if the guess was already made
-        elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+    #     #warning if the guess was already made
+    #     elif(board[guess_row][guess_col] == "X"):
+    #         print "You guessed that one already."
         
-        #if the guess is wrong, mark the point with an X and start again
-        else:
-            print "You missed my battleship!"
-            board[guess_row][guess_col] = "X"
+    #     #if the guess is wrong, mark the point with an X and start again
+    #     else:
+    #         print "You missed my battleship!"
+    #         board[guess_row][guess_col] = "X"
         
-        # Print turn and board again here
-        print "Turn " + str(turn+1) + " out of 4." 
-        print_board(board)
+    #     # Print turn and board again here
+    #     print "Turn " + str(turn+1) + " out of 4." 
+    #     print_board(board)
 
 #if the user have made 4 tries, it's game over
-if turn >= 3:
-    print "Game Over"
+# if turn >= 3:
+#     print "Game Over"
 
 
-def where(playerInput):
-    if "their" in playerInput:
